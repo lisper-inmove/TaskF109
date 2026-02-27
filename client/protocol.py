274a050dcc05569f9528addf09f4e0658aa363bf
inmove <lisper.inmove@gmail.com>
2026-02-28 07:57:40 +0800

    update protocol

--- client/protocol.py
+++ client/protocol.py
@@ -4,8 +4,8 @@ import os
 class ProtocolHeader:
 
     heartbeat = bytes([0xFF, 0x01])
-    set_voltage = bytes([0xFF, 0x01])
-    set_multi_voltage = bytes([0xFF, 0x03])
+    set_voltage = bytes([0xFF, 0x02])
+    start = 0xFF
     end = 0xFE
 
 
@@ -25,7 +25,8 @@ class Protocol:
     def set_voltage(cls, voltage):
         msg = bytearray()
         msg.extend(ProtocolHeader.set_voltage)
-        msg.append(voltage & 0xFF)
+        msg.append(voltage & ProtocolHeader.start)
+        msg.append(0x01)
         msg.append((voltage >> 8) & 0xFF)
         msg.append(ProtocolHeader.end)
         return msg
@@ -33,17 +34,18 @@ class Protocol:
     @classmethod
     def set_multi_voltage(cls, voltages):
         msg = bytearray()
-        msg.extend(ProtocolHeader.set_multi_voltage)
+        msg.extend(ProtocolHeader.set_voltage)
         length = len(voltages)
-        msg.append(length & 0xFF)
+        msg.append(ProtocolHeader.start)
         msg.append((length >> 8) & 0xFF)
+        msg.append(length & 0xFF)
         min_v = int(os.environ.get('VOLTAGE_MIN'))
         max_v = int(os.environ.get('VOLTAGE_MAX'))
         for voltage in voltages:
             if not (min_v <= voltage <= max_v):
                 raise ValueError(
                     f"Voltage {voltage} out of range ({min_v}~{max_v} mV)")
-            msg.append(voltage & 0xFF)
             msg.append((voltage >> 8) & 0xFF)
+            msg.append(voltage & 0xFF)
         msg.append(ProtocolHeader.end)
         return msg
